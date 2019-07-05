826717d0f410b2cf76a2afcae3c5e3d7395ebfb6
saiebmariem <52163812+saiebmariem@users.noreply.github.com>
2019-07-05 10:23:22 +0100

    Add files via upload

---
+++ gomycode/1/js/main.js
@@ -0,0 +1,61 @@
+function bold(){
+    document.getElementById("mytext").style.fontWeight="bold";
+    
+    }
+function itallic(){
+    document.getElementById("mytext").style.fontStyle="italic";
+}
+function underlined1(){
+    document.getElementById("mytext").style.textDecoration="underline";
+}
+function size1(){
+    document.getElementById("mytext").style.fontSize=document.getElementById("three").value;
+}
+function text1(){
+    document.getElementById("mytext").style.fontFamily=document.getElementById("four").value;
+}
+$(".pic1").hover(function(){
+    $(this).animate({opacity:'0.5'} )
+}, function(){
+    $(this).animate({opacity:'1'});
+});
+$(".pic2").hover(function(){
+    $(this).animate({"opacity":".5"} )
+}, function(){
+    $(this).animate({"opacity":"1"});
+})
+$(".pic3").hover(function(){
+    $(this).animate({"opacity":".5"} )
+}, function(){
+    $(this).animate({"opacity":"1"});
+})
+$(".pic4").hover(function(){
+    $(this).animate({"opacity":".5"} )
+}, function(){
+    $(this).animate({"opacity":"1"});
+})
+$(".pic5").hover(function(){
+    $(this).animate({"opacity":".5"} )
+}, function(){
+    $(this).animate({"opacity":"1"});
+})
+$(".pic6").hover(function(){
+    $(this).animate({"opacity":".5"} )
+}, function(){
+    $(this).animate({"opacity":"1"});
+})
+$(".pic7").hover(function(){
+    $(this).animate({"opacity":".5"} )
+}, function(){
+    $(this).animate({"opacity":"1"});
+})
+$(".pic8").hover(function(){
+    $(this).animate({"opacity":".5"} )
+}, function(){
+    $(this).animate({"opacity":"1"});
+})
+$(".pic9").hover(function(){
+    $(this).animate({"opacity":".5"} )
+}, function(){
+    $(this).animate({"opacity":"1"});
+})
